0a285347ec2c60480424627b5c210517e505cfdb
ErrorX <31070311+Nalox1@users.noreply.github.com>
2020-01-12 16:30:17 +0100

    Add files via upload

---
+++ Övningsuppgift2a/src/Fordon.java
@@ -0,0 +1,41 @@
+package Övningsuppgift2a;
+public class Fordon {
+    private Hastighet hastighet;
+    private Vikt vikt;
+    private Ändrahastighet ändrahastighet;
+    public Fordon(){}
+}
+
+class Bil extends Fordon {
+    private Antalväxlar antalväxlar;
+    private Antalväxlaraktiverade antalväxlaraktiverade;
+    private Växla växla;
+
+}
+
+class Båt extends Fordon{
+    private Dödvikt dödvikt;
+    private Sväng sväng;
+
+}
+
+class Tåg extends Fordon{
+    private Antalvagnar antalvagnar;
+    private Kopplavagnar kopplavangar;
+
+}
+
+class Cyckel extends Fordon{
+    private Antalväxlar antalväxlar;
+    private Växlar växlar;
+}
+
+public static void main(String[] args){
+
+}
+
+
+
+
+
+
